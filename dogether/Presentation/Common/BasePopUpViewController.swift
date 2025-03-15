ab86c287e0b48aa9b28ce5d755d0ccffb0d0e5cc
Kim JaeHyung <you3192001@icloud.com>
2025-03-15 23:21:18 +0900

    [UPDATE] 베이스 팝업 뷰컨트롤러 구성 완료 사용법 기제 완료

--- dogether/Presentation/Common/BasePopUpViewController.swift
+++ dogether/Presentation/Common/BasePopUpViewController.swift
@@ -20,14 +20,37 @@ import SnapKit
 ///
 ///     examplePopUpViewController.popUpView.~~Action 접근 가능
 ///
+///     혹은 self.showPopup을 참고해주세요 ( 대리 함수 구성 완료 )
+///
 class BasePopUpViewController<V: UIView>: NotNeedModelBaseViewController {
     
-    let popUpView = V()
+    let popUpView: V
+    
+    private let backgroundView = UIView()
+        .setOf {
+            $0.backgroundColor = UIColor.grey900.withAlphaComponent(0)
+        }
+        
     private var horizontalPadding: CGFloat = 0
     
-    init(horizontalPadding: CGFloat) {
+    private let backGroundDismissOption: Bool
+    
+    init(horizontalPadding: CGFloat, backGroundDismissOption: Bool = true) {
+        self.popUpView = V()
         self.horizontalPadding = horizontalPadding
+        self.backGroundDismissOption = backGroundDismissOption
         super.init(nibName: nil, bundle: nil)
+        self.view.alpha = 0
+        self.popUpView.alpha = 0
+    }
+    
+    init(view: V, horizontalPadding: CGFloat = 0, backGroundDismissOption: Bool = true) {
+        self.popUpView = view
+        self.horizontalPadding = horizontalPadding
+        self.backGroundDismissOption = backGroundDismissOption
+        super.init(nibName: nil, bundle: nil)
+        self.view.alpha = 0
+        self.popUpView.alpha = 0
     }
     
     required init?(coder: NSCoder) {
@@ -41,18 +64,30 @@ class BasePopUpViewController<V: UIView>: NotNeedModelBaseViewController {
     }
     
     override func configureHierarchy() {
+        self.view.addSubview(backgroundView)
         self.view.addSubview(popUpView)
     }
     
     override func configureConstraints() {
+        
+        self.backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
         self.popUpView.snp.makeConstraints {
             $0.horizontalEdges.equalToSuperview().inset(horizontalPadding)
+            $0.center.equalToSuperview()
         }
     }
     
-    override func configureView() {}
-    
+    override func configureView() {
+        
+        disMissCheckToAddAction()
+    }
 }
+
+
+
 // MARK: USE
 extension BasePopUpViewController {
     
@@ -71,14 +106,13 @@ extension BasePopUpViewController {
 extension BasePopUpViewController {
     
     /// 뷰가 사라질때의 애니메이션을 정의합니다.
-    ///
-    /// 직접 호출하셔야 합니다.
-    func dismissAct() {
+    private func dismissAct() {
         view.alpha = 1
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.12) { [weak self] in
             guard let weakSelf = self else { return }
             UIView.animate(withDuration: 0.3) {
                 weakSelf.popUpView.backgroundColor = UIColor.black.withAlphaComponent(0)
+                weakSelf.backgroundView.backgroundColor = UIColor.grey900.withAlphaComponent(0)
                 weakSelf.view.alpha = 0
             } completion: { _ in
                 weakSelf.dismiss(animated: false)
@@ -88,15 +122,55 @@ extension BasePopUpViewController {
     
     /// 팝업 노출 효과를 정의합니다.
     private func showAct() {
-        view.alpha = 0
-        popUpView.alpha = 0
+        backgroundView.backgroundColor = UIColor.grey900.withAlphaComponent(0)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.12) { [weak self] in
             guard let weakSelf = self else { return }
             UIView.animate(withDuration: 0.4) {
-                weakSelf.popUpView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                weakSelf.backgroundView.backgroundColor = UIColor.grey900.withAlphaComponent(0.8)
                 weakSelf.view.alpha = 1
                 weakSelf.popUpView.alpha = 1
             }
         }
     }
+    
+    private func disMissCheckToAddAction() {
+        if backGroundDismissOption {
+            backgroundView
+                .addTapGesture { [weak self] in
+                    guard let self else { return }
+                    dismissAct()
+                }
+        }
+    }
 }
+
+#if DEBUG
+final class ExamplePopupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 버튼 눌렀다 가정
+        let popUpView = CertificationInfoPopupView(todoInfo: TodoInfo(
+            id: 0,
+            content: "CENTER",
+            status: "STATUS"
+        )) {
+            logger.debug("무언가 함.")
+        }
+        
+        // 혹은
+        self.showPopUp(view: popUpView) { view in
+            view.backgroundColor = .red
+            view.dismissButtonTapped()
+            // ..Etc
+        }
+        
+//        // 다음과 같이 단 init() 전용 매개변수가 필요시 위 방식으로 대체 하시오
+//        self.showPopUp(type: CertificationInfoPopupView.self) { view in
+//            view.backgroundColor = .green
+//        }
+    }
+    
+}
+#endif
--- dogether/Utility/Extension/UIViewControllerExt.swift
+++ dogether/Utility/Extension/UIViewControllerExt.swift
@@ -34,4 +34,44 @@ extension UIViewController {
         self.present(popupViewController, animated: animated, completion: completion)
     }
     
+    
+    /// 베이스 팝업 뷰컨트롤러 대리 함수 입니다.
+    /// - Parameters:
+    ///   - type: 팝업 뷰 타입
+    ///   - horizontalPadding: 좌우 패딩값
+    ///   - configuration: 뷰 설정
+    func showPopUp<V: UIView>(
+        type: V.Type,
+        horizontalPadding: CGFloat = 20,
+        backgroundDismissOption: Bool = true,
+        configuration: ((V) -> Void)? = nil
+    ) {
+        let popupVC = BasePopUpViewController<V>(horizontalPadding: horizontalPadding)
+        popupVC.modalPresentationStyle = .overFullScreen
+        popupVC.modalTransitionStyle = .crossDissolve
+        
+        configuration?(popupVC.popUpView)
+        
+        self.present(popupVC, animated: false)
+    }
+    
+    /// 베이스 팝업 뷰컨트롤러 대리 함수 입니다.
+    /// - Parameters:
+    ///   - type: 팝업 뷰 인스턴스
+    ///   - horizontalPadding: 좌우 패딩값
+    ///   - configuration: 뷰 설정
+    func showPopUp<V: UIView>(
+        view: V,
+        horizontalPadding: CGFloat = 20,
+        backgroundDismissOption: Bool = true,
+        configuration: ((V) -> Void)? = nil
+    ) {
+        let popupVC = BasePopUpViewController(view: view, horizontalPadding: horizontalPadding)
+        popupVC.modalPresentationStyle = .overFullScreen
+        popupVC.modalTransitionStyle = .crossDissolve
+        
+        configuration?(popupVC.popUpView)
+        
+        self.present(popupVC, animated: false)
+    }
 }
